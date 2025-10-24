e5b70f2b452fbeb16f45e2ba715e1512b87f5838
PTITer2003 <namyahoo2003@gmail.com>
2025-10-24 11:17:37 +0700

    add reviews module for comments

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,28 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ReviewViewSet
-
-router = DefaultRouter()
-router.register(r"reviews", ReviewViewSet)
+from django.urls import path
+from .views import (
+    ReviewListView,
+    ReviewCreateView,
+    ReviewDetailView,
+    ReviewUpdateView,
+    ReviewDeleteView,
+)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("reviews/", ReviewListView.as_view(), name="review-list"),
+    path(
+        "reviews/create/", ReviewCreateView.as_view(), name="view-create"
+    ),  # POST tạo reviews
+    path(
+        "reviews/<int:pk>/", ReviewDetailView.as_view(), name="view-detail"
+    ),  # GET chi tiết reviews
+    path(
+        "reviews/<int:pk>/update/",
+        ReviewUpdateView.as_view(),
+        name="view-update",
+    ),  # PUT/PATCH cập nhật reviews
+    path(
+        "reviews/<int:pk>/delete/",
+        ReviewDeleteView.as_view(),
+        name="view-delete",
+    ),  # DELETE xóa reviews
 ]
